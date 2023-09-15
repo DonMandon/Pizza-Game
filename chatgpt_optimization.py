dc7e41035fc005c43232f6441194d9ac101086f1
DonMandon <145025996+DonMandon@users.noreply.github.com>
2023-09-15 20:14:02 +0200

    Customer confusion issue patched + ChatGPT test
    - fixed an issue where after the player inputs non-valid coordinates, the customer thinks they didn't order a pizza
    - made a new python file with a version of the code that chatgpt shortened: "This simplified code retains the functionality of your original code while cleaning up some redundant parts and improving readability."
    Note that this version can't handle the player not inputting valid coordinates.

--- chatgpt_optimization.py
+++ chatgpt_optimization.py
@@ -1,18 +1,12 @@
 import random
 
-
-# BASIC game on pdf page 141
 def the_pizza_function():
     pizzas = ['Salami', 'Margherita', 'Diavola', 'Hawaiian', 'Buffalo', 'Pepperoni', 'BBQ', 'Neapolitan', 'Meat']
     toppings = ['salami', 'cheese', 'onions', 'anchovies', 'olives', 'pepperoni', 'BBQ sauce', 'rucola', 'garlic',
                 'pineapple', 'meatballs', 'mushrooms']
     pizza_kind = random.choice(pizzas)
     extra_topping = random.choice(toppings)
-    if random.randint(0, 3) == 3:
-        return f'a {pizza_kind} pizza with extra {extra_topping}'
-    else:
-        return f'a {pizza_kind} pizza'
-
+    return f'a {pizza_kind} pizza with extra {extra_topping}' if random.randint(0, 3) == 3 else f'a {pizza_kind} pizza'
 
 print('''
 ▄▄▄█████▓ ██░ ██ ▓█████     ██▓███   ██▓▒███████▒▒███████▒ ▄▄▄           ▄████  ▄▄▄       ███▄ ▄███▓▓█████ 
@@ -23,10 +17,11 @@ print('''
   ▒ ░░    ▒ ░░▒░▒░░ ▒░ ░   ▒▓▒░ ░  ░░▓  ░▒▒ ▓░▒░▒░▒▒ ▓░▒░▒ ▒▒   ▓▒█░    ░▒   ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░
     ░     ▒ ░▒░ ░ ░ ░  ░   ░▒ ░      ▒ ░░░▒ ▒ ░ ▒░░▒ ▒ ░ ▒  ▒   ▒▒ ░     ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░
   ░       ░  ░░ ░   ░      ░░        ▒ ░░ ░ ░ ░ ░░ ░ ░ ░ ░  ░   ▒      ░ ░   ░   ░   ▒   ░      ░      ░   
-          ░  ░  ░   ░  ░             ░    ░ ░      ░ ░          ░  ░         ░       ░  ░       ░      ░  ░''')
-# Title ASCII art from https://patorjk.com/software/taag/#p=display&f=Bloody&t=THE%20PIZZA%20GAME
+          ░  ░  ░   ░  ░             ░    ░ ░      ░ ░          ░  ░         ░       ░  ░       ░      ░  ░
+''')
+
 player_name = input('What is your first name? ')
-print(f'Hi, {player_name}. In this game you take orders, and then tell a delivery guy where to deliver them.')
+print(f'Hi, {player_name}. In this game, you take orders and tell a delivery guy where to deliver them.')
 print('''
 |Map of the city of Hyattsville|
  -----1-----2-----3-----4-----
@@ -60,100 +55,66 @@ The above is a map of the homes where you need to send pizzas to.
 I recommend you take a picture of it since you'll have to scroll a lot otherwise.
 The letters are the first letters of their names.
 Your job is to give a truck driver the location (the coordinates) of the home ordering the pizza.''')
-introduction_check = input('Do you an introduction? (recommended for a first time player) ')
-if introduction_check.lower() == 'yes':
+introduction_check = input('Do you want an introduction? (recommended for first-time players) ').lower()
+if introduction_check == 'yes':
     print(f'''
 Somebody will ask for a pizza to be delivered. Then a delivery boy will ask you for the location.
 Example: Hello {player_name}'s Pizza. This is Jason. I'd like a pizza.
 Driver to {player_name}. where does Jason live? Your answer would be '2,3'
 (The first coordinate is horizontal, the second one vertical)
 You can quit the game by typing 'quit' when asked about the coordinates.
-A bit sad, but I'm sure they'll find someone else to yell the coordinates for you.''')
-elif introduction_check.lower() == 'no':
-    print("I guess you don't need any more explanations!")
+A bit sad, but I'm sure they'll find someone else to yell the coordinates for you.
+    ''')
+elif introduction_check == 'no':
+    print("You don't need any more explanations!")
 else:
-    print('huh?')
+    print('Huh?')
+
 print('''
 Good luck delivering those pizzas!
 ''')
-caller_names = ['Matt', 'Nolan', 'Oliver', 'Paul', 'Isabelle', 'Jason', 'Kevin', 'Lauren', 'Elizabeth', 'Felix',
-                'Gregory', 'Henry', 'Aaron', 'Barbara', 'Connor', 'David']
+
 caller_coordinates = {
-    'Matt': '1,4',
-    'Nolan': '2,4',
-    'Oliver': '3,4',
-    'Paul': '4,4',
-    'Isabelle': '1,3',
-    'Jason': '2,3',
-    'Kevin': '3,3',
-    'Lauren': '4,3',
-    'Elizabeth': '1,2',
-    'Felix': '2,2',
-    'Gregory': '3,2',
-    'Henry': '4,2',
-    'Aaron': '1,1',
-    'Barbara': '2,1',
-    'Connor': '3,1',
-    'David': '4,1'
+    'Matt': '1,4', 'Nolan': '2,4', 'Oliver': '3,4', 'Paul': '4,4',
+    'Isabelle': '1,3', 'Jason': '2,3', 'Kevin': '3,3', 'Lauren': '4,3',
+    'Elizabeth': '1,2', 'Felix': '2,2', 'Gregory': '3,2', 'Henry': '4,2',
+    'Aaron': '1,1', 'Barbara': '2,1', 'Connor': '3,1', 'David': '4,1'
 }
+
 failed_deliveries = 0
+
 while True:
     pizza_got_cold = False
-    current_caller = random.choice(caller_names)
-    print(f'''
-*ring* *ring* Hello, {player_name}'s Pizza. This is {current_caller}. I'd like a {the_pizza_function()}.
-''')
-    input_coords = str(input(f'Driver to {player_name}. Where does {current_caller} live? '))
+    current_caller = random.choice(list(caller_coordinates.keys()))
+    print(f'*ring* *ring* Hello, {player_name}\'s Pizza. This is {current_caller}. I\'d like {the_pizza_function()}.')
+    input_coords = input(f'Driver to {player_name}. Where does {current_caller} live? ')
+# this version can't handle the player not inputting valid coordinates
     if input_coords.lower() == 'quit':
         break
-    elif caller_coordinates[current_caller] == input_coords:
-        pass
+    elif caller_coordinates.get(current_caller) == input_coords:
+        print(f'*ring* *ring* Hello {player_name}, this is {current_caller}. Thanks for the pizza!')
     else:
         pizza_cold_timer = 0
         while True:
-            try:
-                if caller_coordinates[current_caller] == input_coords:
-                    break
-                else:
-                    print(f'''
-    *ring* *ring* Hello {player_name}, this is {list(caller_coordinates.keys())[list(caller_coordinates.values()).index(input_coords)]}.
-    I did not order a pizza. I live at {input_coords}.''')
-# getting the name from the coordinates from https://stackoverflow.com/questions/8023306/get-key-by-value-in-dictionary
+            if caller_coordinates.get(current_caller) == input_coords:
+                print(f'*ring* *ring* Hello {player_name}, this is {current_caller}. Thanks for the pizza!')
+                break
+            else:
+                print(f'*ring* *ring* Hello {player_name}, this is {list(caller_coordinates.keys())[list(caller_coordinates.values()).index(input_coords)]}. I did not order a pizza. I live at {input_coords}.')
                 input_coords = input(f'Driver to {player_name}. Where does {current_caller} live? ')
-                if caller_coordinates[current_caller] == input_coords:
-                    break
-                else:
-                    pizza_cold_timer += 1
-                    if pizza_cold_timer == 3:
-                        pizza_got_cold = True
-                        break
-                    else:
-                        continue
-            except ValueError:
-                input_coords = input(
-                    f"Driver to {player_name}. These coordinates aren't on the map. Could you repeat that?")
                 pizza_cold_timer += 1
                 if pizza_cold_timer == 3:
                     pizza_got_cold = True
                     break
-                else:
-                    continue
+
     if pizza_got_cold:
-        print(f'''
-Driver to {player_name}. Unfortunately the pizza got cold. I'll have to tell the customer the delivery failed.''')
+        print(f'Driver to {player_name}. Unfortunately, the pizza got cold. The delivery failed.')
         failed_deliveries += 1
         print(f'Failed deliveries: {failed_deliveries}')
         if failed_deliveries == 3:
             break
-        else:
-            continue
-    while True:
-        if caller_coordinates[current_caller] == input_coords:
-            print(f'''
-*ring* *ring* Hello {player_name}, this is {current_caller}. Thanks for the pizza!
-''')
-            break
+
 if failed_deliveries == 3:
-    input('Unfortunately you failed to deliver too many pizzas. You will be... promoted to customer. ')
+    input('Unfortunately, you failed to deliver too many pizzas. You will be... promoted to customer.')
 else:
     input(f'Bye, {player_name}!')
