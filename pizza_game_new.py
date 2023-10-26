ad0d77d0b6dcbadf351880d4a2d8eede0d49cf08
DonMandon <145025996+DonMandon@users.noreply.github.com>
2023-10-26 11:28:00 +0200

    updated function code
    updated it to be less "while True"y

--- pizza_game_new.py
+++ pizza_game_new.py
@@ -2,65 +2,69 @@ import random
 
 
 def main():
-    with open('High_Score.txt', 'w') as f:
-        f.write('')
     player_name = input('What is your first name? ')
     print(f'Hi, {player_name}. In this game, you take orders, and then tell a delivery guy where to deliver them.')
     print('''
-       |Map of the city of Hyattsville|
-        -----1-----2-----3-----4-----
-        -                           -
-        -                           -
-        -                           -
-        -                           -
-        4    M     N     O     P    4
-        -                           -
-        -                           -
-        -                           -
-        -                           -
-        3    I     J     K     L    3
-        -                           -
-        -                           -
-        -                           -
-        -                           -
-        2    E     F     G     H    2
-        -                           -
-        -                           -
-        -                           -
-        -                           -
-        1    A     B     C     D    1
-        -                           -
-        -                           -
-        -                           -
-        -                           -
-        ------1-----2-----3-----4----
+|Map of the city of Hyattsville|
+-----1-----2-----3-----4-----
+-                           -
+-                           -
+-                           -
+-                           -
+4    M     N     O     P    4
+-                           -
+-                           -
+-                           -
+-                           -
+3    I     J     K     L    3
+-                           -
+-                           -
+-                           -
+-                           -
+2    E     F     G     H    2
+-                           -
+-                           -
+-                           -
+-                           -
+1    A     B     C     D    1
+-                           -
+-                           -
+-                           -
+-                           -
+------1-----2-----3-----4----
 
-       The above is a map of the homes where you need to send pizzas to.
-       I recommend you take a picture of it since you'll have to scroll a lot otherwise.
-       The letters are the first letters of their names.
-       Your job is to give a truck driver the location (the coordinates) of the home ordering the pizza.''')
+The above is a map of the homes where you need to send pizzas to.
+I recommend you take a picture of it since you'll have to scroll a lot otherwise.
+The letters are the first letters of their names.
+Your job is to give a truck driver the location (the coordinates) of the home ordering the pizza.
+       ''')
     introduction_check = input('Do you need an introduction? (recommended for a first-time player) ').lower()
     if introduction_check == 'yes':
-        print(f'''
-        Somebody will ask for a pizza to be delivered. Then a delivery boy will ask you for the location.
-        
-        Example: Hello {player_name}'s Pizza. This is Jason. I'd like a pizza.
-        Driver to {player_name}. where does Jason live? Your answer would be '2,3'
-        (The first coordinate is horizontal, the second one vertical)
-        
-        You can quit the game by typing 'quit' when asked about the coordinates.
-        A bit sad, but I'm sure they'll find someone else to yell the coordinates for you.''')
+        print('''
+Somebody will ask for a pizza to be delivered. Then a delivery boy will ask you for the location.
+
+Example: Hello {player_name}'s Pizza. This is Jason. I'd like a pizza.
+Driver to {player_name}. where does Jason live? Your answer would be '2,3'
+(The first coordinate is horizontal, the second one vertical)
+
+You can quit the game by typing 'quit' when asked about the coordinates.
+A bit sad, but I'm sure they'll find someone else to yell the coordinates for you.
+''')
     elif introduction_check == 'no':
         print("Let's get started then.")
     else:
         print('huh?')
-    print('Good luck delivering those pizzas!')
+    print('''Good luck delivering those pizzas!
+''')
     deliver_pizza(player_name)
 
 
 def deliver_pizza(player_name):
-    caller_names = ['Matt', 'Nolan', 'Oliver', 'Paul', 'Isabelle', 'Jason', 'Kevin', 'Lauren', 'Elizabeth', 'Felix',
-                    'Gregory', 'Henry', 'Aaron', 'Barbara', 'Connor', 'David']
+    global game_over
+    global pizza_got_cold
+    global caller_coordinates
+    caller_names = ('Matt', 'Nolan', 'Oliver', 'Paul', 'Isabelle', 'Jason', 'Kevin', 'Lauren', 'Elizabeth', 'Felix',
+                    'Gregory', 'Henry', 'Aaron', 'Barbara', 'Connor', 'David')
     failed_deliveries = 0
     consecutive_deliveries = 0
     while True:
@@ -70,15 +74,16 @@ def deliver_pizza(player_name):
         if input_coords.lower() == 'quit':
             break
         if caller_coordinates.get(current_caller) == input_coords:
-            input(f'*ring* *ring* Hello {player_name}, this is {current_caller}. Thanks for the pizza!')
+            input(f'''*ring* *ring* Hello {player_name}, this is {current_caller}. Thanks for the pizza!
+''')
             consecutive_deliveries += 1
-        else:
+        if pizza_got_cold:
             failed_deliveries += 1
-            print(f'Failed deliveries: {failed_deliveries}')
             consecutive_deliveries = 0
             print(f"Driver to {player_name}. Unfortunately the pizza got cold. I'll have to tell the customer the delivery failed.")
             input(f'Failed deliveries: {failed_deliveries}')
             if failed_deliveries >= 3:
+                game_over = True
                 break
     end_game(player_name, consecutive_deliveries)
 
@@ -96,6 +101,8 @@ def the_pizza_function():
 
 
 def get_delivery_coordinates(player_name, current_caller):
+    global pizza_got_cold
+    global caller_coordinates
     failed_deliveries= 0
     while True:
         input_coords = input(f'Driver to {player_name}. Where does {current_caller} live? ')
@@ -104,7 +111,11 @@ def get_delivery_coordinates(player_name, current_caller):
         elif caller_coordinates.get(current_caller) == input_coords:
             break
         else:
-            print(f"*ring* *ring* Hello {player_name}, this is {get_caller_by_coordinates(caller_coordinates, input_coords)}. I did not order a pizza. I live at {input_coords}.")
+            if get_caller_by_coordinates(caller_coordinates, input_coords) is None:
+                # add easter egg here
+                print('coordinates not valid')
+            else:
+                print(f"*ring* *ring* Hello {player_name}, this is {get_caller_by_coordinates(caller_coordinates, input_coords)}. I did not order a pizza. I live at {input_coords}.")
             failed_deliveries += 1
             if failed_deliveries >= 3:
                 pizza_got_cold = True
@@ -118,16 +129,19 @@ def get_caller_by_coordinates(caller_coordinates_for_function, input_coords):
 
 
 def end_game(player_name, consecutive_deliveries):
+    global game_over
     with open('High_Score.txt') as f:
-        high_score = int(f.read())
+        try:
+            high_score = int(f.read())
+        except ValueError:
+            high_score = 0
     if consecutive_deliveries > high_score:
         with open('High_Score.txt', 'w') as f:
             f.write(str(consecutive_deliveries))
         print(f'New High Score!: {consecutive_deliveries}')
-    if failed_deliveries >= 3:
+    if game_over:
         input('Unfortunately you failed to deliver too many pizzas. You will be... promoted to a customer. ')
     input(f'Bye, {player_name}!')
-
 
 
 caller_coordinates = {
@@ -148,6 +162,8 @@ caller_coordinates = {
     'Connor': '3,1',
     'David': '4,1'
 }
+failed_deliveries = 0
 pizza_got_cold = False
+game_over = False
 if __name__ == "__main__":
     main()
